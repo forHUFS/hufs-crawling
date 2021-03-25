4c1a2aacfe3678663972e498a4b4cb301b882a5e
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-03-25 22:26:09 +0900

    [ 이태현 ] 기능 수정: Driver Options

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from crawling.scholarship import Scholarship
 
 
 def lambda_handler(event, context):
-    print(event['body'])
-    # Scholarship.insert_scholarship_data()
+    scholarship = Scholarship()
+    scholarship.insert_scholarship_data()
 
-    # required_data     = Scholarship.get_scholarship_data()
-    # scholarship_slack = Slack(channel = '10-게시판-관리', content = required_data)
-    # response          = scholarship_slack.post_message()
+    required_data     = scholarship.get_scholarship_data()
+    scholarship_slack = Slack(channel = '10-게시판-관리', content = required_data)
+    response          = scholarship_slack.post_message()
 
-    # return response
+    return response
+
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
