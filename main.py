c97bf24cb33be442a3a51c76b9abee2d0e3cb150
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-05-06 20:14:10 +0900

    Add Requirements

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ def lambda_handler(event, context):
     scholarship = Scholarship()
     scholarship.insert_scholarship_data()
 
-    required_data     = scholarship.get_scholarship_data()
-    scholarship_slack = Slack(channel = '10-게시판-관리', content = required_data)
-    response          = scholarship_slack.post_message()
-
-    return response
+    # required_data     = scholarship.get_scholarship_data()
+    # scholarship_slack = Slack(channel = '10-게시판-관리', content = required_data)
+    # response          = scholarship_slack.post_message()
 
 
 if __name__ == '__main__':
