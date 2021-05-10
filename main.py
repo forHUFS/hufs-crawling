e02e9c5c948e0d33b539414a51ec3f645372e30e
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-05-10 22:15:38 +0900

    Fix: Lambda Function에서 수행 가능하게 Chrome Options 수정

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ from crawling.scholarship import Scholarship
 
 def lambda_handler(event, context):
     scholarship = Scholarship()
-    scholarship.insert_scholarship_data()
+    # scholarship.insert_scholarship_data()
 
-    # required_data     = scholarship.get_scholarship_data()
-    # scholarship_slack = Slack(channel = '10-게시판-관리', content = required_data)
-    # response          = scholarship_slack.post_message()
-
-
-if __name__ == '__main__':
-    lambda_handler(None, None)
+    required_data     = scholarship.get_scholarship_data()
+    scholarship_slack = Slack(channel = '10-게시판-관리', content = required_data)
+    response          = scholarship_slack.post_message()
+    print(response)
--- worker/driver.py
+++ worker/driver.py
@@ -1,5 +1,4 @@
-import os
-import pathlib
+import os, stat, pathlib
 
 from selenium                          import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -98,35 +97,28 @@ class DriverUtils:
 
 
 class WebDriver(webdriver.Chrome, DriverUtils):
-    def __init__(self, file: str = 'chromedriver'):
-        path = pathlib.Path(file).absolute()
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--single-process')
-        options.add_argument('--disable-dev-shm-sage')
-        # chrome_options = Options()
-        # chrome_options.add_argument('--headless')
-        # chrome_options.add_argument('--no-sandbox')
-        # chrome_options.add_argument('--disable-gpu')
-        # chrome_options.add_argument('--window-size=1280x1696')
-        # chrome_options.add_argument('--user-data-dir=/tmp/user-data')
-        # chrome_options.add_argument('--hide-scrollbars')
-        # chrome_options.add_argument('--enable-logging')
-        # chrome_options.add_argument('--log-level=0')
-        # chrome_options.add_argument('--v=99')
-        # chrome_options.add_argument('--single-process')
-        # chrome_options.add_argument('--data-path=/tmp/data-path')
-        # chrome_options.add_argument('--ignore-certificate-errors')
-        # chrome_options.add_argument('--homedir=/tmp')
-        # chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
-        # chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
-        # chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
+    def __init__(self, file: str = 'mac_chromedriver'):
+        path           = '/opt/python/bin/chromedriver'
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--window-size=1280x1696')
+        chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+        chrome_options.add_argument('--hide-scrollbars')
+        chrome_options.add_argument('--enable-logging')
+        chrome_options.add_argument('--log-level=0')
+        chrome_options.add_argument('--v=99')
+        chrome_options.add_argument('--single-process')
+        chrome_options.add_argument('--data-path=/tmp/data-path')
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--homedir=/tmp')
+        chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+        chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+        chrome_options.binary_location = "/opt/python/bin/headless-chromium"
 
         webdriver.Chrome.__init__(
             self,
             executable_path = path,
-            options         = options
+            options         = chrome_options
         )
-
-    
